edea24433f05c6f60fdb1d4c9c3c85ed28ce1ee6
MoreThanHidden <john816_@hotmail.com>
2018-02-27 00:22:16 +0800

    Initial commit

---
+++ src/main/java/morethanhidden/adaptivestorage/adaptivestorage.java
@@ -0,0 +1,28 @@
+package morethanhidden.adaptivestorage;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.API;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+    @Mod(modid = adaptivestorage.MODID, name = adaptivestorage.NAME, version = adaptivestorage.VERSION)
+    public class adaptivestorage {
+        public static final String MODID = "adaptive-storage";
+        public static final String NAME = "Adaptive Storage";
+        public static final String VERSION = "1.0";
+
+        private static Logger logger;
+
+        @EventHandler
+        public void preInit(FMLPreInitializationEvent event){
+            logger = event.getModLog();
+        }
+
+        @EventHandler
+        public void init(FMLInitializationEvent event){
+
+        }
+    }
